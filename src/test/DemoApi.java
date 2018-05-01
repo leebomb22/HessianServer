69b80156838301f2a4b3821e778842ffc45ce283
leebomb22 <insistingll@gmail.com>
2018-05-01 12:48:14 +0800

    test

---
+++ src/test/DemoApi.java
@@ -0,0 +1,5 @@
+package test;
+
+public interface DemoApi {
+    public void sayHello();
+}
---
+++ src/test/DemoService.java
@@ -0,0 +1,8 @@
+package test;
+
+public class DemoService implements DemoApi{
+    @Override
+    public void sayHello() {
+        System.out.println("hello,i'm lilei.");
+    }
+}
